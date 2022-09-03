e629270ffe9c213b8ed7d45380e7bb0ac54873b5
Grant Greer <viridiang93@gmail.com>
2022-09-03 16:27:02 -0700

    checking linter

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 const FeedbackCard = () => {
   return (
@@ -8,4 +8,4 @@ const FeedbackCard = () => {
   );
 };
 
-export default FeedbackCard;
+export default FeedbackCard
